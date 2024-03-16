f4c61bb964e2ff1182ff7485ad8f8cb44c4ffdec
zawkindev <zawkindev@gmail.com>
2024-03-16 13:00:44 +0500

    feat: add another theme

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # monochrome-configs
-![""](screenshot.png)
+
+##### orklv:
+![""](orzklv/orzlkv.png)
+
+#### shahruz:
+![""](shahruz/shahruz.png)
 
 - **OS:** [`Arch Linux 'x86_64'`](https://archlinux.org/)
 - **Terminal:** [`alacritty`](https://github.com/alacritty/alacritty)
 - **Bar:** [`polybar`](https://github.com/polybar/polybar)
 - **Compositor:** [`picom`](https://github.com/yshui/picom)
 - **Screenshot tool:** [`flameshot`](https://flameshot.org)
-- **Hotkey daemon:** [`sxhkd`](https://github.com/baskerville/sxhkd)
-- **Fonts:** [`CommitMono Nerd Font`](https://www.nerdfonts.com/font-downloads)
+- **Fonts:** [`CommitMono Nerd Font`](https://www.nerdfonts.com/font-downloads), [`CodeNewRoman Nerd Font`](https://www.nerdfonts.com/font-downloads)
 
-## Theme founder: [@orzklv](https://github.com/orzklv)
+Install the themes: 
+```
+python setup.py
+```
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+
+themes = ["orzklv", "shahruz"]
+
+
+def configure(index):
+    os.system(f"cp {themes[index]}/* ~/.config -r")
+    os.system("mkdir -p ~/.font")
+    os.system("cp fonts/* ~/.fonts -r")
+    os.system("cp dunst tmux wallpaper.jpg ~/.config -r")
+
+
+theme_question = ""
+for index, element in enumerate(themes):
+    theme_question += f"[{index}] {element}        "
+
+try:
+    print("Which theme do you want to install?\n")
+    print(theme_question)
+
+    theme_index = int(input("Enter theme index: "))
+    if theme_index >= len(themes):
+        print(f"\nEnter only 1..{len(themes)}!")
+        exit()
+
+    packages = ""
+    with open("requirements.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            packages += line.strip() + " "
+
+    if len(packages) > 0:
+        os.system(f"sudo pacman -S {packages}")
+        configure(theme_index)
+
+except ValueError:
+    print(f"Enter only 0..{len(themes)-1}")
+
+except FileNotFoundError as e:
+    print(f"\n{e.filename} not found!")
